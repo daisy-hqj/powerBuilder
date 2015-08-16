2b0921e9e141c215aaf5eb93432f2042d2154c76
明几 <qiujia.hqj@alipay.com>
2015-08-16 13:40:22 +0800

    form

--- h5-offline-package/index.js
+++ h5-offline-package/index.js
@@ -1,13 +1,28 @@
 var exp = require('express');
+var bodyParser = require('body-parser');
 
 var app = exp();
 
-app.set('views',  __dirname + '/template');
-app.set('view engine', 'ejs');
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+app.set('views',  __dirname + '/template');
+//模板引擎
+app.set('view engine', 'ejs');
+//渲染文件
 app.get('/', function(req, res){
     res.render('global');
 });
+//表单路径
+app.post('/submit', function(req, res){
+    res.render('global');
+    console.log(req.body);
+});
+//静态资源
+app.use(exp.static(__dirname + '/static'));
 
 if (!module.parent) {
   app.listen(3000);
