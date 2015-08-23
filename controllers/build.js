92a24a3e6ac2a3f6381b54bd02cc9b3d0a479e82
hadesdu <denghongqi@gmail.com>
2015-08-23 21:47:24 +0800

    modify build

--- controllers/build.js
+++ controllers/build.js
@@ -1,7 +1,48 @@
+var db = require('../dao/db');
+var conf = require('../conf/main');
+var download = require('../lib/download').download;
+
+var path = require('path');
+var fs = require('fs');
+
 exports.build = function (req, res) {
     var query = req.query;
     query.ids = query.ids || '';
 
     var arr = query.ids.split(',');
-    res.end('build');
+
+    db.queryUrls(arr, function (data) {
+        data = data || {};
+
+        var urls = [];
+
+        data.forEach(function (item, idx) {
+            if (urls.indexOf(item.url) === -1) {
+                urls.push(item.url);
+            }
+        });
+
+        var buildPath = getPath();
+
+        download(urls, buildPath).then(
+            function () {
+                // do some build
+                res.end('build done');
+            },
+            function () {
+                res.end('build error');
+            }
+        );
+    });
 };
+
+function getPath() {
+
+    var buildPath = Math.ceil(Math.random() * 10000000000) + '';
+
+    while(fs.existsSync(path.join(conf.buildConf.dir, buildPath))) {
+        buildPath = Math.ceil(Math.random() * 10000000000) + '';
+    }
+
+    return path.join(conf.buildConf.dir, buildPath);
+}
